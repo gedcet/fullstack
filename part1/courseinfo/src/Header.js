6ce4e461688efc216ca5c96d5e38bd8b0843d21b
gedcet <gedcet@gmail.com>
2022-03-14 09:51:45 +0200

    1.1

---
+++ part1/courseinfo/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props1) =>
+{
+    return (
+        <div>
+            <h1>{props1.go}</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Total = (props1) =>
+{
+    return (
+        <div>
+            <p>Number of exercises {props1.go}</p>
+        </div>
+    )
+}
+
+export default Total;
