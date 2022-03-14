7428170fcb808f8f284a842820310be4584bafe4
gedcet <gedcet@gmail.com>
2022-03-14 10:49:43 +0200

    1.4

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -5,9 +5,9 @@ const Content = (props) =>
 {
     return (
         <div>
-            <Part part={props.part1} exercises={props.exercises1} />
-            <Part part={props.part2} exercises={props.exercises2} />
-            <Part part={props.part3} exercises={props.exercises3} />
+            <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+            <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+            <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
         </div>
     )
 }
--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -4,7 +4,7 @@ const Total = (props1) =>
 {
     return (
         <div>
-            <p>Number of exercises {props1.totalnumber}</p>
+            <p>Total number {props1.parts[0].exercises + props1.parts[1].exercises + props1.parts[2].exercises}</p>
         </div>
     )
 }
