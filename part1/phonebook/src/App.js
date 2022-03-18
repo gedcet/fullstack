8a6a05f4026ecbe45bb312bb96e122f64100e48c
gedcet <gedcet@gmail.com>
2022-03-18 10:35:21 +0200

    2.6

--- part1/phonebook/src/App.js
+++ part1/phonebook/src/App.js
@@ -5,17 +5,38 @@ const App = () =>
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const handleClick = (event) =>
+  {
+    event.preventDefault();//neperkrauna puslapio
+    //console.log(event.target)
+    const arrayCopy = [...persons];
+    arrayCopy.push({ name: newName });
+    setPersons(arrayCopy);
+  }
+  const handleChange = (event) =>
+  {
+    //console.log(event.target.value);//
+    setNewName(event.target.value);
+  }
+  const sAm = [];
+  for (let i = 0; i < persons.length; i++)
+  {
+    sAm.push(<p key={i}>{persons[i].name}</p>);
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onChange={handleChange} onSubmit={handleClick}>
         <div> name: <input /></div>
-        <div>number: <input /></div>
+        {/* <div>number: <input /></div> */}
         <div> <button type="submit" >add</button>  </div>
       </form>
-      <h2>Numbers</h2>
-      <div>Numbers {newName}</div>
-      
+      <h2>Numbers </h2>
+      {sAm}
+      <div></div>
+
     </div>
   )
 }
