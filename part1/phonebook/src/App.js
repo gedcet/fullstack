ed4026e5b9ec6d7ae698e482512bb86c332c3cb4
gedcet <gedcet@gmail.com>
2022-03-18 15:44:00 +0200

    2.9

--- part1/phonebook/src/App.js
+++ part1/phonebook/src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import Client from './Clients'
 
 const App = () =>
 {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '888-888'}])
-  const [newNumber, setNewNumber] = useState('')
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newFilterValue, setNewFilterValue] = useState('')
   const [newFilterValueNumber, setNewFilterValueNumber] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newName, setNewName] = useState('')
 
   const handleClick = (event) =>
   {
@@ -26,54 +33,45 @@ const App = () =>
     arrayCopyPersons.push({ name: newName, number: newNumber });
     setPersons(arrayCopyPersons);
   }
+
   const handleChangeName = (event) =>
-  {
-    //console.log(event.target.value);//
+  {//console.log(event.target.value);//
     setNewName(event.target.value);
   }
 
   const handleChangeNumber = (event) =>
-  {
-    setNewNumber(event.target.value);
-  }
-  
+  { setNewNumber(event.target.value); }
+
   const handleFilter = (event) =>
-  {
-       setNewFilterValue(event.target.value);
-  }
+  { setNewFilterValue(event.target.value); }
 
   const handleFilterNumber = (event) =>
-  {
-       setNewFilterValueNumber(event.target.value);
-  }
-  
-  const showPersonNames = [];
-  for (let i = 0; i < persons.length; i++)
-  {
-    const regEx1 = new RegExp(newFilterValue, 'i');
-    const regEx2 = new RegExp(newFilterValueNumber, 'i');
-    if (regEx1.test(persons[i].name) && (regEx2.test(persons[i].number)))  
-    {
-      showPersonNames.push(<p key={i}>Personalijos vardas: <strong>{persons[i].name}</strong> telefonas <strong>{persons[i].number}</strong> </p>);
-    }
+  { setNewFilterValueNumber(event.target.value); }
 
+  const clearFilter = () =>
+  {
+    setNewFilterValue("")
+    setNewFilterValueNumber("")
   }
-  showPersonNames.push("Nieko nerasta");
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <p>filter by name <input onChange={handleFilter} /></p>
-      <p>filter by number <input onChange={handleFilterNumber} /></p>
+      {/* <p>filter by name <input onChange={handleFilter} /></p>
+      <p>filter by number <input onChange={handleFilterNumber} /></p> */}
       <div></div>
       <form onSubmit={handleClick}>
-        <div> Name: <input onChange={handleChangeName} /></div>
-        <div> Number: <input onChange={handleChangeNumber} /></div>
-        <div> <button type="submit" >add</button>  </div>
+        <div>Name: <input onChange={handleChangeName} /> filter by name <input onChange={handleFilter} /></div>
+        <div> Number: <input onChange={handleChangeNumber} />filter by number <input onChange={handleFilterNumber} /></div>
+        <div> <button type="submit" >Add</button>  </div>
       </form>
-      <h2>Numbers </h2>
-      {showPersonNames}
-      <div></div>
+      {/* <div> <button onClick={clearFilter} >Clear filters</button>  </div> bandziau clear butona padaryti*/}
+      <hr></hr>
+      <h2>Clients:</h2>
+      <h1><strong>Vardas, Pavarde: </strong>     Telefonas:</h1>
+      <hr size="10"></hr>
+      <Client persons={persons} newFilterValue={newFilterValue} newFilterValueNumber={newFilterValueNumber} />
+
 
     </div>
   )
---
+++ part1/phonebook/src/Clients.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const Client = function (props)
+{
+    //console.log(props);
+    const showPersonNames = [];
+    const regEx1 = new RegExp(props.newFilterValue, 'i');
+    const regEx2 = new RegExp(props.newFilterValueNumber, 'i');
+
+    for (let i = 0; i < props.persons.length; i++)
+    {
+        if (regEx1.test(props.persons[i].name) && (regEx2.test(props.persons[i].number)))  
+        {
+            showPersonNames.push(<p key={i}><strong>{props.persons[i].name}</strong>  {props.persons[i].number}</p>);
+        }
+    }
+    if (showPersonNames.length === 0)
+    {
+        showPersonNames.push(<p key={0}>Nieko nerasta </p>);
+    }
+    return (
+        <div>
+            {showPersonNames}
+        </div>)
+}
+
+export default Client
