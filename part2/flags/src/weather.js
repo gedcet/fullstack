e610b0a4ff77223e08b75c7d88d954b6ebd8bffb
gedcet <gedimina.cet@gmail.com>
2022-03-21 22:36:31 +0200

    2.11 neveikia miesto temp

--- part2/flags/src/weather.js
+++ part2/flags/src/weather.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
 import axios from "axios";
+import { useEffect, useState } from 'react'
 
 const Weather = (props) =>
 {
@@ -7,7 +7,8 @@ const Weather = (props) =>
     let api_URL_weather = 'https://api.openweathermap.org/data/2.5/weather?q='
     let api_miestas = props.capital[0]
     let api_appid = "&appid="
-    const api_key = process.env.REACT_APP_API_KEY
+    //const api_key = process.env.REACT_APP_API_KEY
+    const api_key = "c5a6378ecaf139175755b915beae46f0"
     
     const fetchWeather = async () =>
     {
@@ -15,7 +16,7 @@ const Weather = (props) =>
         {
             const url = `${api_URL_weather}${api_miestas}${api_appid}${api_key}`
             const result = await axios.get(url);
-            CapitalTemp = result.data.main.temp/32
+            CapitalTemp = (result.data.main.temp)/32
             console.log(CapitalTemp)
             
         }
