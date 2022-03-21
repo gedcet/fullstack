539e8c214e5bd76a9844bf6c4f814d11c70c2dbf
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2022-03-21 20:27:33 +0200

    fix

--- part2/flags/src/weather.js
+++ part2/flags/src/weather.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
 
 const Weather = (props) =>
 {
-    const [CapitalTemp, setCapitalTemp] = useState("")
+    const [CapitalTemp, setCapitalTemp] = React.useState("")
     let api_URL_weather = 'https://api.openweathermap.org/data/2.5/weather?q='
     let api_miestas = props.capital[0]
     let api_appid = "&appid="
     const api_key = process.env.REACT_APP_API_KEY
-    
+
+    //https://stackoverflow.com/questions/19477324/how-do-i-calculate-the-temperature-in-celsius-returned-in-openweathermap-org-jso
+    const kelvin_to_celsius = function (kelvin)
+    {
+        return kelvin - 273.15;
+    }
+
     const fetchWeather = async () =>
     {
         try
         {
             const url = `${api_URL_weather}${api_miestas}${api_appid}${api_key}`
             const result = await axios.get(url);
-            CapitalTemp = result.data.main.temp/32
-            console.log(CapitalTemp)
-            
+            // console.log(result.data);
+            const celsius = kelvin_to_celsius(result.data.main.temp);
+            setCapitalTemp(celsius);
         }
         catch (err)
         {
@@ -25,11 +31,11 @@ const Weather = (props) =>
         }
     }
 
-useEffect(fetchWeather, [])
+    React.useEffect(fetchWeather, [])
 
     return (
         <div>
-            {CapitalTemp+""} C
+            {parseFloat(CapitalTemp).toFixed(2)} C
         </div>
     )
 }
