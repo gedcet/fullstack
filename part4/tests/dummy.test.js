4af86ae176591006069e2e74f4bfbc8f18a9a232
gedcet <gedcet@gmail.com>
2022-04-15 15:32:15 +0300

    4.3 zingsnis

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -1,9 +1,9 @@
-const dummy = require('../utils/list_helper.js')
+const a = require('../utils/list_helper.js')
+const totalLikes = require('../utils/list_helper.js')
 
-test('dummy return One', () => 
+test('dummy return vienas', () => 
 {
     const blogs = []
-
-    const result = dummy(blogs)
+    const result = a.dummy(blogs)
     expect(result).toBe(1)
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,6 +1,22 @@
+const Blog = require("../models/modelBlog");
+
 const dummy = (blogs) =>
 {
   return 1;
 }
 
-module.exports = dummy
+const totalLikes = () =>
+{
+  try
+  {
+    result1 = Blog()
+    console.log(result1)
+  }
+  catch (err)
+  {
+    console.log("totalu skaiciavimo klaida " + err)
+  }
+}
+
+//module.exports = { "dummy": dummy, "totalLikes": totalLikes }
+module.exports = { dummy, totalLikes }
